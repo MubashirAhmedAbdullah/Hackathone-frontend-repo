70f99a4bcdf2c96fdca72cab15e01847cbb2809f
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2025-02-02 17:20:45 +0500

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import UserNavbar from './user/userLayout/userLayout'
 import UserDashboard from './user/userDashboard'
 import UserLoans from './user/userLoans'
 import LoanDetails from './user/loanDetailsPage'
+import Login from './auth/login'
+import Signup from './authentication/signUp'
 function App() {
 
   return (
